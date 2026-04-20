0109ae799297139d9571620977b45e1ea64c80d2
Tarmacruel <jonatassousa@outlook.com>
2026-04-20 12:33:22 -0300

    Corrige carregamento e preenchimento automatico no modal de multas

--- frontend/src/api/vehicles.js
+++ frontend/src/api/vehicles.js
@@ -3,6 +3,7 @@ import api from './client'
 export const vehiclesAPI = {
   list: (params) => api.get('/vehicles', { params }),
   listPaginated: (params) => api.get('/vehicles/paginated', { params }),
+  currentDriver: (id) => api.get(`/vehicles/${id}/current-driver`),
   create: (data) => api.post('/vehicles', data),
   update: (id, data) => api.put(`/vehicles/${id}`, data),
   remove: (id) => api.delete(`/vehicles/${id}`),
